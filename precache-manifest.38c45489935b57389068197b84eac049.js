152dd0115fdd14f38cf187ad740d278c3338706e
Paula Santos <paulasantos@Paulas-MBP.lan>
2023-05-11 21:49:49 +0100

    Updates

---
+++ precache-manifest.38c45489935b57389068197b84eac049.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "51458720c365fd257af4",
+    "url": "/art-ritas-corner/static/js/main.d1ad1b54.chunk.js"
+  },
+  {
+    "revision": "679233e0b39b6ce4df71",
+    "url": "/art-ritas-corner/static/js/runtime~main.3ff3a525.js"
+  },
+  {
+    "revision": "55b74c8ceaed7b416105",
+    "url": "/art-ritas-corner/static/js/2.0010ee2b.chunk.js"
+  },
+  {
+    "revision": "d89c3cd02b239c7ae9cd72fc77e4f156",
+    "url": "/art-ritas-corner/index.html"
+  }
+];
