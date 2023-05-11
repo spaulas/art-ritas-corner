3bef47f5aadc6ef04c780da6e58d7f8efafe29f6
Paula Santos <paulasantos@Paulas-MBP.lan>
2023-05-11 22:06:06 +0100

    Change Router

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Details from "../pages/Details";
 import Main from "../pages/Main";
 
