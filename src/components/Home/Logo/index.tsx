7650169173485e423eae8a8758e122a931acdcb5
Paula Santos <paulasantos@Paulas-MBP.lan>
2023-05-12 16:44:57 +0100

    Add images

--- src/components/Home/Logo/index.tsx
+++ src/components/Home/Logo/index.tsx
@@ -1,12 +1,20 @@
 import React from "react"
 import Circle from "components/Home/Logo/Circle"
+import logo from "assets/logo.png"
+import branch from "assets/branch.png"
 import "./styles.scss"
 
 const Logo = () => {
-    return <div className="circles">
-        <Circle color="blue" />
-        <Circle color="pink" />
-    </div>
+    return (
+        <div className="logo-container">
+            <img className="branch" src={branch} alt="branch" />
+            <div className="circles">
+                <Circle color="blue" />
+                <Circle color="pink" />
+            </div>
+            <img className="logo" src={logo} alt="logo" />
+        </div>
+    )
 }
 
 export default Logo;
