db12b3a7b7d0a070a6040a62ceb194e6d3b4ff8c
Paula Santos <paulasantos@Paulas-MBP.lan>
2023-05-13 09:31:27 +0100

    Create data json for categories

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 
-const Category = () => {
-    return <div>Category</div>
-}
+type Text = {
+  fr: string;
+  en: string;
+};
+
+type Image = {
+  title: Text;
+  description: Text;
+  price?: number;
+  duration?: number;
+  src: string;
+};
+
+type Blob = {
+  color: string;
+  id: number;
+};
+
+type CategoryProps = {
+  title: Text;
+  description: Text;
+  flower: number;
+  images: Image[];
+  blobs: Blob[];
+};
+
+const Category = ({
+  title,
+  description,
+  flower,
+  images,
+  blobs,
+}: CategoryProps) => {
+  return <div>Category</div>;
+};
 
 export default Category;
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-import Home from 'components/Home'
-import "./styles.scss"
+import Home from "components/Home";
+import data from "data.json";
+import "./styles.scss";
+import Category from "components/Category";
 
 const Main = () => {
-    return <div className="main-page"><Home/></div>
-}
+  const renderCategories = () => {
+    Object.values(data.categories).map((category) => (
+      <Category {...category} />
+    ));
+    return null;
+  };
+
+  return (
+    <div className="main-page">
+      <Home />
+      {renderCategories()}
+    </div>
+  );
+};
 
 export default Main;
