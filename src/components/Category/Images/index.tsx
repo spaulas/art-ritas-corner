f66a0c013b17f0861e39b93b36545c4991d6950a
Paula Santos <paulasantos@Paulas-MBP.lan>
2023-06-20 20:41:20 +0100

    Add slider to the right

--- src/components/Category/Images/index.tsx
+++ src/components/Category/Images/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import type { Text } from "../Title";
 import "./styles.scss";
 
@@ -20,37 +20,86 @@ type ImageProps = {
 
 const Images = ({ images, isHoverRight }: ImageProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [marginLeft, setMarginLeft] = useState(0);
+
+  const isSliding = useRef(false);
+  const startX = useRef(0);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const IMAGES_VISIBLE_ON_HOVER = 4;
 
   const hoverImages = [...images].slice(0, IMAGES_VISIBLE_ON_HOVER);
-  const clickImages = [...images].slice(IMAGES_VISIBLE_ON_HOVER, images.length);
+  const currentImages = isOpen ? images : hoverImages;
 
-  const hoverImagesClassName = classNames("hover-images", {
-    "hover-right": !isOpen && isHoverRight,
-    "hover-left": !isOpen && !isHoverRight,
-    "open-right": isOpen && isHoverRight,
-    "open-left": isOpen && !isHoverRight
-  });
-  const clickImagesClassName = classNames("click-images", {
-    "open-right": isOpen && isHoverRight,
-    "open-left": isOpen && !isHoverRight
-  });
+  const handleMouseDown = (e: any) => {
+    if (!isOpen) return;
+
+    isSliding.current = true;
+    startX.current = e.pageX + marginLeft / -3;
+  };
+
+  const handleOnMouseLeave = () => {
+    if (!isOpen) return;
+    isSliding.current = false;
+  };
+
+  const handleMouseUp = () => {
+    if (!isOpen) return;
+    isSliding.current = false;
+  };
+
+  const handleOnMouseMove = (e: any) => {
+    if (!isOpen || !isSliding.current) return;
+
+    e.preventDefault();
+
+    let _marginLeft = (e.pageX - startX.current) * 3;
+    if (_marginLeft > 0) {
+      _marginLeft = 0
+    } else if (
+      containerRef?.current &&
+      _marginLeft < containerRef.current.clientWidth / -2
+    ) {
+      _marginLeft = containerRef.current.clientWidth / -2
+    } 
+
+    setMarginLeft(_marginLeft)
+  };
+
+  const imagesClassName = classNames(
+    "images-slider",
+    `container-width-${currentImages.length}`,
+    {
+      "hover-right": !isOpen && isHoverRight,
+      "hover-left": !isOpen && !isHoverRight,
+      "open-right": isOpen && isHoverRight,
+      "open-left": isOpen && !isHoverRight,
+      active: isSliding.current,
+    }
+  );
 
   return (
-    <div className="images-container">
-      <div className={hoverImagesClassName} onClick={() => setIsOpen(true)}>
-        {hoverImages.map((image, index) => (
-          <Card image={image} zIndex={IMAGES_VISIBLE_ON_HOVER - index} isOpen={isOpen} />
+    <div
+      className="images-container"
+      onMouseDown={handleMouseDown}
+      onMouseLeave={handleOnMouseLeave}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleOnMouseMove}
+    >
+      <div
+        ref={containerRef}
+        className={imagesClassName}
+        onClick={() => setIsOpen(true)}
+        style={{ marginLeft }}
+      >
+        {currentImages.map((image, index) => (
+          <Card
+            image={image}
+            zIndex={IMAGES_VISIBLE_ON_HOVER - index}
+            isOpen={isOpen}
+          />
         ))}
       </div>
-      {clickImages && isOpen ? (
-        <div className={clickImagesClassName}>
-          {clickImages.map((image) => (
-            <Card image={image} isOpen={isOpen}  />
-          ))}
-        </div>
-      ) : null}
     </div>
   );
 };
