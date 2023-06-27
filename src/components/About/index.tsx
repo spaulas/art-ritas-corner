cf59e2e1d016a0c3b300a77f361e68ff9685ce25
Paula Santos <paulasantos@Paulas-MBP.lan>
2023-06-27 07:39:26 +0100

    Finish about page

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Picture from "./Picture";
 import "./styles.scss";
+import bottomRectangle from "assets/bottomRectangle.png";
+import flower02 from "assets/Flowers/flower02.png";
+import flower06 from "assets/Flowers/flower06.png";
 
 type AboutProps = {
   profileText: string;
@@ -10,6 +13,12 @@ const About = ({ profileText }: AboutProps) => {
   return (
     <div className="about-page">
       <Picture text={profileText} />
+      <div className="about-flowers-container">
+        <img className="about-flower" src={flower02} alt="flower02" />
+        <img className="about-flower" src={flower06} alt="flower06" />
+      </div>
+      <div className="about-bottom">
+        <img className="bottom-rectangle" src={bottomRectangle} alt="bottomRectangle" /></div>
     </div>
   );
 };
