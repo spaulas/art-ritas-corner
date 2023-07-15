86aa312fe651b4f7761e75ecbbe3efac910a2093
Paula Santos <paulasantos@Paulas-MBP.lan>
2023-07-15 14:06:56 +0100

    Add first email

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,10 @@
-import React, { ReactElement, useContext, useEffect, useState } from "react";
+import React, {
+  ReactElement,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import Button from "components/common/Button";
 import blobBlue03 from "assets/Blobs/blob_blue_03.png";
 import blobCream09 from "assets/Blobs/blob_cream_09.png";
@@ -10,6 +16,7 @@ import NailsPrices from "./NailsPrices";
 import NailsTC from "./NailsTC";
 import NailsDisclaimerCheckbox from "./Nails/DisclaimerCheckbox";
 import { DisclaimersContext } from "context/DisclaimersProvider";
+import emailjs from "@emailjs/browser";
 
 type FormElements = {
   form: ReactElement;
@@ -19,22 +26,52 @@ type FormElements = {
 };
 
 const ContactForm = () => {
-  const { basicFields, updateBasicFields, updateNailsFields } = useContext(FormContext);
+  const { basicFields, nailsFields, updateBasicFields, updateNailsFields } =
+    useContext(FormContext);
   const { hasAcceptedNailsTCOnce, setHasAcceptedNailsTCOnce } =
     useContext(DisclaimersContext);
   const [formElements, setFormElements] = useState<FormElements>();
 
+  const temp = useRef<HTMLFormElement>(null);
+
   const onAgreeNailsDisclaimer = () => {
     if (!hasAcceptedNailsTCOnce) {
       setHasAcceptedNailsTCOnce(true);
-      updateNailsFields({disclaimer: true})
+      updateNailsFields({ disclaimer: true });
     }
     updateBasicFields({ type: "nails" });
   };
 
   const onSubmitPaintingMessage = () => {};
 
-  const onSubmitNailAppointmentRequest = () => {};
+  const onSubmitNailAppointmentRequest = () => {
+    emailjs
+      .send(
+        "art-rita-corner--test",
+        "nailsTemplate",
+        {
+          name: basicFields.name,
+          email: basicFields.email,
+          phoneNumber: basicFields.phone,
+          services: nailsFields.services.toString(),
+          duration: 0,
+          price: 0,
+          date: "23/07/2023",
+          schedule: "10h00-11h00",
+          notes: "Notes!",
+          images: []
+        },
+        "QdGvtgrmrTek7hpRv"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   useEffect(
     function getFormElements() {
@@ -88,10 +125,12 @@ const ContactForm = () => {
         <img className="form-blob-blue" src={blobBlue03} alt="form-blob-blue" />
       </div>
       <div className="form-box">
-        <div className="form-title">Contactez moi</div>
-        <Selector />
-        {formElements.form}
-        {formElements.checkbox ?? null}
+        <form ref={temp}>
+          <div className="form-title">Contactez moi</div>
+          <Selector />
+          {formElements.form}
+          {formElements.checkbox ?? null}
+        </form>
         {formElements.buttonTitle && formElements.onSubmit ? (
           <Button
             title={formElements.buttonTitle}
