7c45797821d29691b53d2b2d14f120f36722e5dd
Paula Santos <paulasantos@Paulas-MBP.lan>
2023-07-15 10:42:02 +0100

    refactor form elements

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { ReactElement, useContext, useEffect, useState } from "react";
 import Button from "components/common/Button";
 import blobBlue03 from "assets/Blobs/blob_blue_03.png";
 import blobCream09 from "assets/Blobs/blob_cream_09.png";
@@ -9,21 +9,60 @@ import "./styles.scss";
 import NailsPrices from "./NailsPrices";
 import NailsTC from "./NailsTC";
 
+type FormElements = {
+  formComponent: ReactElement;
+  buttonTitle?: string;
+  onSubmit?: () => void;
+};
+
 const ContactForm = () => {
   const { basicFields } = useContext(FormContext);
+  const [formElements, setFormElements] = useState<FormElements>();
 
-  const renderView = () => {
-    switch (basicFields.type) {
-      case "nailsPrices":
-        return <NailsPrices />;
-      case "nailsTC":
-        return <NailsTC />;
-      case "paintings":
-      case "nails":
-      default:
-        return <BasicForm />;
-    }
-  };
+  const onAgreeNailsDisclaimer = () => {};
+
+  const onSubmitPaintingMessage = () => {};
+
+  const onSubmitNailAppointmentRequest = () => {};
+
+  useEffect(
+    function getFormElements() {
+      switch (basicFields.type) {
+        case "nailsPrices":
+          setFormElements({
+            formComponent: <NailsPrices />,
+          });
+          break;
+        case "nailsTC":
+          setFormElements({
+            formComponent: <NailsTC />,
+            buttonTitle: "Je suis d'accord",
+            onSubmit: onAgreeNailsDisclaimer,
+          });
+          break;
+        case "nails":
+          setFormElements({
+            formComponent: <BasicForm />,
+            buttonTitle: "Reservez-le",
+            onSubmit: onSubmitNailAppointmentRequest,
+          });
+          break;
+        case "paintings":
+        default:
+          setFormElements({
+            formComponent: <BasicForm />,
+            buttonTitle: "Envoyer le message",
+            onSubmit: onSubmitPaintingMessage,
+          });
+          break;
+      }
+    },
+    [basicFields.type]
+  );
+
+  if (!formElements) {
+    return null;
+  }
 
   return (
     <div className="form-page" id="form-page">
@@ -38,11 +77,13 @@ const ContactForm = () => {
       <div className="form-box">
         <div className="form-title">Contactez moi</div>
         <Selector />
-        {renderView()}
-        <Button
-          title="Reservez-le"
-          onClick={() => console.log("send form info")}
-        />
+        {formElements.formComponent}
+        {formElements.buttonTitle && formElements.onSubmit ? (
+          <Button
+            title={formElements.buttonTitle}
+            onClick={formElements.onSubmit}
+          />
+        ) : null}
       </div>
     </div>
   );
